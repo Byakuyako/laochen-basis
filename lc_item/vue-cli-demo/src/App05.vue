f5dca2aa3470646575b9980824d0898c9c6705a5
Jesse <dynaptx@163.com>
2020-09-01 22:29:28 +0800

    组件插值新建

---
+++ lc_item/vue-cli-demo/src/App05.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h1>{{msg}} 欢迎你</h1>
+    <children @changeParent="editUsername"></children>
+  </div>
+</template>
+
+<script>
+import Children from './components/Children'
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      msg: "Jesse"
+    }
+  },
+  methods: {
+    editUsername: function (e) {
+      this.msg = e
+    }
+  },
+  components: {
+    Children
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
