6e75971993c4d12f05bddfb1bb3bc5daba754b07
Jesse <dynaptx@163.com>
2020-09-01 17:39:35 +0800

    完成props部分

---
+++ lc_item/vue-cli-demo/src/App03.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <label>
+      用户名: <input type="text" placeholder="请输入用户名" v-model="username">
+    </label>
+    <h1>用户名: {{username}}</h1>
+
+    <label>复选框<input type="checkbox" v-model="check"></label>
+    <h1>check: {{check}}</h1>
+
+    <label>
+      <textarea cols="30" rows="2" v-model="article"></textarea>
+    </label>
+    <h1>article: {{article}}</h1>
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      msg: "hello word",
+      username: '',
+      check: false,
+      article: ''
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- lc_item/vue-cli-demo/src/App04.vue
+++ lc_item/vue-cli-demo/src/App04.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-
+    <h1>{{msg}} 欢迎你</h1>
+    <hello-world user="老陈" :brief="brief"></hello-world>
+    <hello-world user="小陈" :brief="brief"></hello-world>
+    <hello-world user="胖子" :brief="brief"></hello-world>
   </div>
 </template>
 
@@ -11,8 +14,15 @@ export default {
   name: 'App',
   data: function () {
     return {
-      msg: "laochen"
+      msg: "laochen",
+      brief: {
+        title: '老陈很强',
+        desc: '臭不要脸'
+      }
     }
+  },
+  components: {
+    HelloWorld
   }
 }
 </script>
